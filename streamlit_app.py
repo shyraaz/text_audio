a8342d134e8c615d0d23b0a49ad8926a21e9f47a
shyraaz <67177864+shyraaz@users.noreply.github.com>
2025-08-12 17:39:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,21 @@
 import streamlit as st
-from transformers import pipeline 
-import io
-import soundfile as sf
-import numpy as np
-
-import streamlit as st
 from transformers import pipeline
 
 # عنوان التطبيق
 st.title("sentiment analysis")
 
-# إعداد الـ pipelines
-sentiment_pipe = pipeline("sentiment-analysis")
-tts_pipe = pipeline("text-to-speech", model="suno/bark-small", device="cpu")
+# إعداد الـ pipeline
+sentiment_pipe = pipeline(
+    "sentiment-analysis",
+    model="distilbert/distilbert-base-uncased-finetuned-sst-2-english"
+)
 
 # إدخال النص
 user_text = st.text_input("أدخل النص هنا:")
 
 # زر التنفيذ
-if st.button("حلل النص و اسمع الصوت"):
+if st.button("حلل النص"):
     # تحليل المشاعر
     sentiment_result = sentiment_pipe(user_text)
     st.write("نتيجة تحليل المشاعر:")
     st.write(sentiment_result)
-    
-    # تحويل النص إلى صوت
-    audio_result = tts_pipe(user_text)
-    st.audio(audio_result["audio"], format="audio/wav")
-
- 
